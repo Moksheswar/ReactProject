3013c7540903ac9783502fd2253d54a306f9c02f
Moksheswar Palla <pallamoksheswar692@gmail.com>
2024-10-22 23:19:55 +0530

    Currency Converter

---
+++ currencyconverter/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Inputdiv from './components/inputdiv'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+function App() {
+  const [amount, setAmount] = useState()
+  const [convertedAmount, setConvertedAmount] = useState()
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+  
+  const convert = () => {
+    if (amount && currencyInfo[to]) {
+    setConvertedAmount(amount * currencyInfo[to])
+    }
+    else {
+      setConvertedAmount(null); // Handle case where the conversion rate is unavailable
+    }
+  }
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    // setConvertedAmount(amount)
+    // setAmount(convertedAmount)
+  }
+
+  return (
+    <div className='w-full h-screen flex flex-col justify-center items-center bg-no-repeat bg-cover space-y-5' 
+    style={{backgroundImage:'url(https://img.freepik.com/premium-photo/3d-illustration-stack-gold-coins-with-forex-chart-stock-market-graph-background_795422-6851.jpg)'}}>
+      <h1 className='text-white text-2xl'>Currency Converter</h1>
+      <div className='bg-white w-96 p-10 rounded-lg shadow-lg backdrop-blur-sm backdrop-filter bg-opacity-30 flex flex-col space-y-4 items-center'>
+        <Inputdiv 
+        label="From" 
+        amount={amount}
+        currencyOptions={options}
+        onCurrencyChange={(currency) => setFrom(currency)}
+        onAmountChange={(amount) => setAmount(amount)}
+        selectedCurrency={from}
+        />
+        <button 
+        onClick={swap}
+        className='bg-blue-600 text-white w-20 rounded-md p-1 text-sm'
+        >SWAP</button>
+        <Inputdiv 
+        label="To"
+        amount={convertedAmount}
+        currencyOptions={options}
+        onCurrencyChange={(currency) => setTo(currency)}
+        selectedCurrency={to}
+        amountDisabled
+        />
+        <button 
+        onClick={convert}
+        className='bg-blue-600 w-full text-white rounded-md p-3 text-lg'>
+        CONVERT</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ currencyconverter/src/components/inputdiv.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+// const id= useId()
+function inputdiv({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = 'usd',
+  amountDisabled = false,
+  currencyDisabled = false,
+  className = ''
+}) {
+  return (
+    <div className='bg-white w-full h-20 rounded-lg py-2 px-3 space-y-1'>
+      <div className='text-gray-700 flex flex-wrap justify-between'>
+        <label htmlFor="currency">{label}</label>
+        <label htmlFor="Type">Type</label>
+      </div>
+      <div className='flex flex-wrap justify-between'>
+        <input id="currency" type="number"
+        placeholder="Enter amount"
+        value={amount}
+        disabled={amountDisabled}
+        onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+        />
+        <select
+        value={selectedCurrency}
+        disabled={currencyDisabled}
+        onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default inputdiv
